ba003629a92df11502e63a4a8f7fc8ffcad0dc79
JACPro <jamesacpro@gmail.com>
2021-02-18 08:24:30 +0000

    Refactored CubeEditor and Waypoint scripts to reflect appropriate dependencies

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
-
-    TextMesh label;
+    Waypoint waypoint;
+    int gridSize;
 
     void Awake() {
-        label = GetComponentInChildren<TextMesh>();            
+        waypoint = GetComponent<Waypoint>();
+        gridSize = waypoint.GetGridSize();
     }
 
     void Update()
     {
-        Vector3 snapPos;
-        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        SnapToGrid();
+        UpdateLabel();        
+    }
 
+    private void SnapToGrid()
+    {
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x, 
+            0f,
+            waypoint.GetGridPos().y
+        );
+    }
 
-        transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
-
-        //update cube label
-        string labelText = snapPos.x / gridSize + "," + snapPos.z / gridSize;
+    private void UpdateLabel()
+    {
+        TextMesh label = GetComponentInChildren<TextMesh>();
+        string labelText = 
+            waypoint.GetGridPos().x / gridSize + 
+            "," + 
+            waypoint.GetGridPos().y / gridSize;
         gameObject.name = "Cube " + labelText;
         label.text = labelText;
     }
